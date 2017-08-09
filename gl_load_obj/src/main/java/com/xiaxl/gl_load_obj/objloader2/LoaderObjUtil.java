16a93575e3ea27c4b9192be119ca907a85146749
xiaxueliang <bjxiaxueliang@corp.netease.com>
2017-08-09 15:29:39 +0800

    [ojb加载]删除错误obj

--- gl_load_obj/src/main/java/com/xiaxl/gl_load_obj/objloader2/LoaderObjUtil.java
+++ gl_load_obj/src/main/java/com/xiaxl/gl_load_obj/objloader2/LoaderObjUtil.java
@@ -344,6 +344,7 @@ public class LoaderObjUtil {
          * 顶点、纹理、法向量一一对应后的数据
          */
         public float[] aVertices;
+        // 顶点纹理可能会没有
         public float[] aTexCoords;
         public float[] aNormals;
 
